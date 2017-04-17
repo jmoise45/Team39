f7f014cf3d0ca261b234aa530e6add8236edb282
ssimon419 <ssimon419@ufl.edu>
2017-04-16 22:54:22 -0400

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,16 +1,27 @@
 #ifndef PERSON_H
 #define PERSON_H
+
 #include <string>
 #include <vector>
+
 using namespace std;
 
+/*
+ *  ==========================================================
+ * 		Person Class Declarations
+ * ==========================================================
+ 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 */
 class Person
 {
     public:
+        /* 	    Mutator Methods 	    */
         void setName(std::string &name);
+    
+        /* 	    Accessor Methods    	*/
         std::string getName();
         int getAge();
-    protected:
+    
+    protected: // indicates that the below data are only accesible by this class or those that implement it
         std::string name;
         int age;
         std::string jobTitle;
@@ -18,26 +29,52 @@ class Person
         bool isGoing;
 };
 
-class Guest: public Person
+
+/*
+ *  ==========================================================
+ * 		Guest Class Declarations
+ * ==========================================================
+ 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 */
+class Guest: public Person // Declares that 'Guest' class implements 'Person' class
 {
     public:
+        /*      Constructor	        	*/
         Guest(std::string &name, int &age);
+    
+        /*  	Mutator Methods	    	*/
         void setAge(int &age);
+        
+        /*      Useful Methods          */
         void RSVP(bool &coming);
+    
+        /*  	Accessor Methods	   	*/
         bool getRSVPStatus();
         int getAge();
+    
+        
 };
 
+
+/*
+ *  ==========================================================
+ * 		PersonList Class Declarations
+ * ==========================================================
+ 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 */
 class personList
 {
     private:
         vector<Person*> personlist;
 
     public:
+        /*      Constructor	        	*/
         personList();
+    
+        /*      Useful Methods          */
         void addPerson(Person *person);
         void deletePerson(std::string personName);
         void printList();
+     
+        /*  	Accessor Methods	   	*/
         unsigned int getSize();
 };
 
