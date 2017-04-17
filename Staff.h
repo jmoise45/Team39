5f92f47fa0204a847264e92c38a6a73f5150404e
ssimon419 <ssimon419@ufl.edu>
2017-04-16 22:46:57 -0400

    Update Staff.h

--- Staff.h
+++ Staff.h
@@ -1,14 +1,28 @@
 #ifndef STAFF_H_INCLUDED
 #define STAFF_H_INCLUDED
+
 #include <string>
 #include <vector>
-class Staff: public Person {
-public:
-    Staff(string jobTitle, double hireCost);
-    void setJobTitle(string jobTitle);
-    void setHireCost(double hireCost);
-    string getJobTitle();
-    double getHireCost();
+
+/*
+ *  ==========================================================
+ * 		Staff Class Declarations
+ * ==========================================================
+ 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 */
+
+class Staff: public Person // Declares that 'Staff' implements 'Person'
+{
+    public:
+        /*      Constructor	        	*/
+        Staff(string jobTitle, double hireCost);
+        
+        /*  	Accessor Methods	    	*/
+        void setJobTitle(string jobTitle);
+        void setHireCost(double hireCost);
+    
+        /*  	Mutator Methods	    	*/
+        string getJobTitle();
+        double getHireCost();
 };
 
 #endif // STAFF_H_INCLUDED
