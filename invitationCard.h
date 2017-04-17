26ba3c7706f0840095b48db24eddf3696d19026f
ssimon419 <ssimon419@ufl.edu>
2017-04-16 22:42:55 -0400

    Update invitationCard.h

--- invitationCard.h
+++ invitationCard.h
@@ -1,5 +1,9 @@
 #ifndef INVITATIONCARD_H_INCLUDED
 #define INVITATIONCARD_H_INCLUDED
-#include <cctype>
+
+#include <cctype> // allows for character conversions (i.e. uppercase->lowercase)
+
+/*      Initializes invitation card               */
 void invitationCard(string partyName, string date, string venueLocation, string time, string venue);
+
 #endif // INVITATIONCARD_H_INCLUDED
